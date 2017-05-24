ca88399a1c43991722c52ec1ccdac10855466ba6
SinaKhalili <sinakhalili@outlook.com>
2017-05-24 13:51:58 -0700

    first commit

---
+++ List.h
@@ -0,0 +1,56 @@
+/*
+ * List.h
+ * 
+ * Class Description: List data collection ADT.
+ * Class Invariant: Data collection with the following characteristics:
+ *                   - Each element is unique (no duplicates).
+ *                   - (What other characteristic does our List have?)
+ *
+ * Last modified on: May 2017
+ * Author: AL
+ */
+
+#pragma once
+#include <string>
+#include "Patient.h"
+
+using namespace std;
+
+
+class List  {
+
+private:
+	static const int MAX_ELEMENTS = 3; // Small capacity so can test when data collection becomes full
+	                                   // As we are testing the code of our assignment, we can change the value given to this constant.
+	Patient elements[MAX_ELEMENTS];    // Data structure with capacity of MAX_ELEMENTS
+	int elementCount;                  // Current element count in element array
+	int capacity;                      // Actual maximum capacity of element array
+      
+public:
+	// Default constructor
+	List();
+
+	// Description: Returns the total element count currently stored in List.
+	int  getElementCount() const;
+
+	// Description: Insert an element.
+	// Precondition: newElement must not already be in data collection.  
+	// Postcondition: newElement inserted and elementCount has been incremented.   
+	bool insert(const Patient& newElement);
+
+	// Description: Remove an element. 
+	// Postcondition: toBeRemoved is removed and elementCount has been decremented.	
+	bool remove( const Patient& toBeRemoved );
+	
+	// Description: Remove all elements.
+	void removeAll();
+   
+	// Description: Search for target element.
+	//              Returns a pointer to the element if found,
+	//              otherwise, returns NULL.
+	Patient* search(const Patient& target);
+   
+	// Description: Prints all elements stored in List.
+	void printList( );
+
+}; // end List
---
+++ Patient.h
@@ -0,0 +1,80 @@
+/*
+ * Patient.h
+ * 
+ * Class Description: Models a walk-in clinic patient.
+ * Class Invariant: Each patient has a unique care card number.
+ *                  This care card number must have 10 digits.
+ *                  This care card number cannot be modified. 
+ *
+ * Last modified on: May 2017
+ * Author: AL
+ */
+
+#pragma once
+#include <string>
+using namespace std;
+
+class Patient {
+	
+private:
+	string name;                
+	string address;          
+	string phone;
+	string email;
+	string careCard;
+
+public:
+	// Default Constructor
+	// Description: Create a patient with a care card number of "0000000000". 
+	// Postcondition: All data members set to "To be entered", 
+	//                except the care card number which is set to "0000000000".       
+	Patient();
+
+	// Parameterized Constructor
+	// Description: Create a patient with the given care card number.
+	// Postcondition: If aCareCard does not have 10 digits, then care card is set to "0000000000".
+	//                All other data members set to "To be entered".
+	Patient(string aCareCard);
+
+	// Getters and setters
+	// Description: Returns patient's name.
+	string getName() const;
+
+	// Description: Returns patient's address.
+	string getAddress() const;
+
+	// Description: Returns patient's phone number.
+	string getPhone() const;
+
+	// Description: Returns patient's email.
+	string getEmail() const;
+	
+	// Description: Returns patient's care card number.
+	string getCareCard() const;
+
+	// Description: Sets the patient's name.
+	void setName(const string aName);
+
+	// Description: Sets the patient's address.
+	void setAddress(const string anAddress);
+
+	// Description: Sets the patient's phone number.
+	void setPhone(const string aPhone);
+
+	// Description: Sets the patient's email.
+	void setEmail(const string anEmail);
+
+	// Overloaded Operators
+	// Description: Comparison operator. Compares "this" Patient object with "rhs" Patient object.
+	//              Returns true if both Patient objects have the same care card number.
+	bool operator==(const Patient & rhs);
+	
+	// Description: Greater than operator. Compares "this" Patient object with "rhs" Patient object.
+	//              Returns true if the care card number of "this" Patient object is > the care card number of "rhs" Patient object.
+	bool operator>(const Patient & rhs);
+	
+	// Description: Prints the content of "this" patient.
+	void printPatient( );
+
+};
+// end of Patient.h
