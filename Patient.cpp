eeb9cb8e49a61ab3a018a5fbb4feaaacbbe30e64
SinaKhalili <sinakhalili@outlook.com>
2017-05-25 15:32:00 -0700

    default constructor

---
+++ Patient.cpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <string>
+using namespace std;
+#include "Patient.h"
+
+	// Default Constructor
+	// Description: Create a patient with a care card number of "0000000000". 
+	// Postcondition: All data members set to "To be entered", 
+	//                except the care card number which is set to "0000000000".       
+	Patient::Patient(): careCard("0000000000"),name("To be entered"), address("To be entered"), 
+						email("To be entered"), phone("To be entered"){}
+
+
+	// Parameterized Constructor
+	// Description: Create a patient with the given care card number.
+	// Postcondition: If aCareCard does not have 10 digits, then care card is set to "0000000000".
+	//                All other data members set to "To be entered".
+	Patient::Patient(string aCareCard);
+
+	// Getters and setters
+	// Description: Returns patient's name.
+	string getName() const;
+
+	// Description: Returns patient's address.alavergn
+	string getAddress() const;
+
+	// Description: Returns patient's phone number.
+	string getPhone() const;
+
+	// Description: Returns patient's email.
+	string getEmail() const;
+	
+	// Description: Returns patient's care card number.
+	string getCareCard() const;
+
+	// Description: Sets the patient's name.
+	void setName(const string aName);
+
+	// Description: Sets the patient's address.
+	void setAddress(const string anAddress);
+
+	// Description: Sets the patient's phone number.
+	void setPhone(const string aPhone);
+
+	// Description: Sets the patient's email.
+	void setEmail(const string anEmail);
+
+	// Overloaded Operators
+	// Description: Comparison operator. Compares "this" Patient object with "rhs" Patient object.
+	//              Returns true if both Patient objects have the same care card number.
+	bool operator==(const Patient & rhs);
+	
+	// Description: Greater than operator. Compares "this" Patient object with "rhs" Patient object.
+	//              Returns true if the care card number of "this" Patient object is > the care card number of "rhs" Patient object.
+	bool operator>(const Patient & rhs);
+	
+	// Description: Prints the content of "this" patient.
+	void printPatient( );
